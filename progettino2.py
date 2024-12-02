7b3083d0c6ea70e4398f7125832a9b8e1a662f15
andrea1932734 <andreacarovelli@gmail.com>
2024-12-02 19:07:57 +0000

    finee

--- progettino2.py
+++ progettino2.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect 
+from flask_sqlalchemy import SQLAlchemy #
 from models import db, ListaSpesa
 
+#creo un app flask
 app = Flask(__name__)
+
+# Configura il database SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lista_spesa.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -11,11 +14,15 @@ db.init_app(app)
 with app.app_context():
     db.create_all()  
 
+
+# Route principale
 @app.route('/')
 def home():
     elementi = ListaSpesa.query.all()  
     return render_template('index.html', elementi=elementi)
 
+
+# Route per aggiungere un nuovo elemento alla lista
 @app.route('/aggiungi', methods=['POST'])
 def aggiungi():
     nuovo_elemento = request.form['elemento'] 
@@ -24,6 +31,8 @@ def aggiungi():
     db.session.commit() 
     return redirect('/')
 
+
+# Route per rimuovere un elemento dalla lista (identificato dal suo ID)
 @app.route('/rimuovi/<int:id>', methods=['POST'])
 def rimuovi(id):
     elemento = ListaSpesa.query.get_or_404(id)  
@@ -31,6 +40,8 @@ def rimuovi(id):
     db.session.commit()  
     return redirect('/')
 
+
+# Route per svuotare l'intera lista della spesa
 @app.route('/svuota', methods=['POST'])
 def svuota():
     ListaSpesa.query.delete() 
